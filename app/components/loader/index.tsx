fc0239e1c762bd8ce6f9b308cd2b8d47c67de8e4
george-hub331 <idiaghegeorge9@gmail.com>
2024-04-11 07:21:31 +0100

    Update loader component with new logo and progress bar

--- app/components/loader/index.tsx
+++ app/components/loader/index.tsx
@@ -1,13 +1,17 @@
-import { Box, CircularProgress } from "@mui/material";
-import logo from "../../../public/images/logo.png"
+import Box from "@mui/material/Box";
+import LinearProgress from "@mui/material/LinearProgress";
+import logo from "../../../public/images/icon.png"
 import Image from "next/image";
+import { Line } from "recharts";
 
 const Loader = ({
   text = "",
   fixed = true,
+  incLogo = true,
   sx = {},
 }: {
   text?: any;
+  incLogo?: boolean;
   fixed?: boolean;
   sx?: object;
 }) => {
@@ -18,14 +22,27 @@ const Loader = ({
         justifyContent: text ? "space-around" : "center",
         ...sx,
       }}
-      className="h-full left-0 bg-[#2626263a] backdrop-blur-[4px] top-0 z-[9999999099999999999999999] flex flex-col justify-center items-center w-full"
+      className="h-full left-0 bg-[#26262633] backdrop-blur-[4px] top-0 z-[999999909999999999999999] flex flex-col justify-center items-center w-full"
     >
+      {incLogo && (
+        <div className="items-center flex justify-center">
+          <Image src={logo} alt="Clover" width={90} />
+        </div>
+      )}
+
       <Box
-        className="text-[#ff5555] mt-11 justify-center "
+        className="text-[#ff5555] h-5 mt-11 justify-center "
         sx={{ display: "flex" }}
       >
-        <CircularProgress size={text ? 80 : 60} color="inherit" />
+        {/* <CircularProgress size={text ? 80 : 60} color="inherit" /> */}
+        <LinearProgress variant="indeterminate"  color="inherit" sx={{
+          width: 90,
+          height: 5,
+          borderRadius: 3,
+          backgroundColor: "rgba(255, 85, 85, 0.1)",
+        }} />
       </Box>
+
       {text !== "" && (
         <div className="text-[#ff5555ff] text-xl font-bold">{text}</div>
       )}
